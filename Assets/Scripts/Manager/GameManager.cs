8f85299418f396cf8077ed851cd5cef1fe7ca52d
zumak475206 <wkzum2006@gmail.com>
2026-01-01 19:05:17 +0700

    enemy, score , wrong

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("UI Score & Wrong")]
+    [SerializeField] private TextMeshProUGUI wrongKeyText;
+    [SerializeField] private TextMeshProUGUI scoreText; // Kéo Text điểm số vào đây
+
+    private int wrong = 0;
+    private int score = 0;
+
+    void Start()
+    {
+        UpdateUI();
+    }
+
+    // --- XỬ LÝ LỖI (WRONG) ---
+    public void AddWrong(int amount)
+    {
+        wrong += amount;
+        UpdateUI();
+    }
+
+    // --- XỬ LÝ ĐIỂM (SCORE) - Mới thêm vào ---
+    public void AddScore(int amount)
+    {
+        score += amount;
+        UpdateUI();
+        Debug.Log($"Cộng {amount} điểm. Tổng: {score}");
+    }
+
+    private void UpdateUI()
+    {
+        if (wrongKeyText != null) wrongKeyText.text = "Wrong: " + wrong.ToString();
+        if (scoreText != null) scoreText.text = "Score: " + score.ToString();
+    }
+}
---
+++ Assets/Scripts/Manager/KeyBoardCheck.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using TMPro;
+public class KeyBoardCheck : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro textMesh;
+    private string allowedChars = "abcdefghijklmnopqrstuvwxyz";
+
+    // BIẾN QUAN TRỌNG: Để các script khác biết con này đang giữ chữ gì
+    public char myChar;
+
+    void Start()
+    {
+        SetRandomLetter();
+    }
+
+    void SetRandomLetter()
+    {
+        int randomIndex = Random.Range(0, allowedChars.Length);
+        myChar = allowedChars[randomIndex]; // Lưu chữ cái vào biến
+        textMesh.text = myChar.ToString();  // Hiển thị lên đầu
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.localScale.x < 0)
+        {
+            textMesh.transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            textMesh.transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+}
